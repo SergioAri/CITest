255855090ec04ffa25129caa5c828eea6636f6ad
IW <iw@macbook-pro-de-iw.local>
2016-05-05 17:50:13 -0500

    New

--- CITestUITests/CITestUITests.swift
+++ CITestUITests/CITestUITests.swift
@@ -19,8 +19,15 @@ class CITestUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    func testLabelValue() {
+        
+        let app = XCUIApplication()
+        XCTAssert(app.staticTexts["CI Test"].exists)
+        
     }
     
     override func tearDown() {
